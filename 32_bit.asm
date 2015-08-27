f955f24af3b5c07dd21f9a2042fb15aafdb590a0
shubhamchh <shubham.iiith@gmail.com>
2015-08-27 13:51:48 +0530

    Create 32_bit.asm

---
+++ 32_bit.asm
@@ -0,0 +1,58 @@
+	.file	"add.c"
+	.intel_syntax noprefix
+	.text
+	.globl	simple_l
+	.type	simple_l, @function
+simple_l:
+.LFB0:
+	.cfi_startproc
+	push	ebp
+	.cfi_def_cfa_offset 8
+	.cfi_offset 5, -8
+	mov	ebp, esp
+	.cfi_def_cfa_register 5
+	sub	esp, 16
+	mov	eax, DWORD PTR [ebp+8]
+	mov	edx, DWORD PTR [eax]
+	mov	eax, DWORD PTR [ebp+12]
+	add	eax, edx
+	mov	DWORD PTR [ebp-4], eax
+	mov	eax, DWORD PTR [ebp+8]
+	mov	edx, DWORD PTR [ebp-4]
+	mov	DWORD PTR [eax], edx
+	mov	eax, DWORD PTR [ebp-4]
+	leave
+	.cfi_restore 5
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	simple_l, .-simple_l
+	.globl	main
+	.type	main, @function
+main:
+.LFB1:
+	.cfi_startproc
+	push	ebp
+	.cfi_def_cfa_offset 8
+	.cfi_offset 5, -8
+	mov	ebp, esp
+	.cfi_def_cfa_register 5
+	sub	esp, 16
+	mov	DWORD PTR [ebp-8], 5
+	push	12
+	lea	eax, [ebp-8]
+	push	eax
+	call	simple_l
+	add	esp, 8
+	mov	DWORD PTR [ebp-4], eax
+	mov	eax, 0
+	leave
+	.cfi_restore 5
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+.LFE1:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 4.9.2-0ubuntu1~14.04) 4.9.2"
+	.section	.note.GNU-stack,"",@progbits
